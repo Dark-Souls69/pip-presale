41a4e9341b2873525f09eff1a78ed567f6f532f0
Dark-Souls69 <alvamima@gmail.com>
2025-07-07 22:30:17 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function copyAddress() {
-  const addressText = document.getElementById("contract-address").innerText;
-  navigator.clipboard.writeText(addressText)
-    .then(() => alert("Contract address copied!"))
-    .catch(() => alert("Failed to copy address."));
-}
